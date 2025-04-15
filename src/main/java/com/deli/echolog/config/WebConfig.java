ebd63d81742446a6ec61738ff10b1f0657735231
wooooo <playjiwoo@naver.com>
2025-04-15 16:02:46 +0900

    Webconfig 모든 요청 허용

--- src/main/java/com/deli/echolog/config/WebConfig.java
+++ src/main/java/com/deli/echolog/config/WebConfig.java
@@ -10,9 +10,9 @@ public class WebConfig implements WebMvcConfigurer {
     @Override
     public void addCorsMappings(CorsRegistry registry) {
         registry.addMapping("/**")
-                .allowedOrigins("http://localhost:8081")  // 모든 도메인에서 요청을 허용
-                .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS")  // 허용할 HTTP 메서드
-                .allowedHeaders("*")  // 모든 헤더 허용
-                .allowCredentials(true);  // 자격증명 사용하지 않음
+                .allowedOriginPatterns("*") // 모든 경로 요청 허용
+                .allowedMethods("*")
+                .allowedHeaders("*")
+                .allowCredentials(true);
     }
 }

6c5f04fcea1722bf57f9d7eb96c09bb60e2ee506
wooooo <playjiwoo@naver.com>
2025-05-02 16:09:16 +0900

    일기 목록 요청할 때 연,월 입력받는거 추가함

--- src/main/java/com/deli/echolog/repository/DiaryRepository.java
+++ src/main/java/com/deli/echolog/repository/DiaryRepository.java
@@ -4,11 +4,20 @@ import com.deli.echolog.domain.Diary;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.time.LocalDate;
 import java.util.List;
 
 @Repository
 public interface DiaryRepository extends JpaRepository<Diary, Long> {
 
-    // 일기 목록 조회
+    // 모든 일기 목록 조회
     List<Diary> findByMemberId(Long memberId);
+
+    // 일기 월별로 조회
+    List<Diary> findByMemberIdAndWrittenDateBetween(Long memberId, LocalDate startDate, LocalDate endDate);
+
+
+
+
+
 }

21cfdd9127f7810c906f05e0a7ae3323ca058d1c
wooooo <playjiwoo@naver.com>
2025-05-06 18:10:38 +0900

    recap 기능 추가함

---
+++ src/main/java/com/deli/echolog/repository/RecapRepository.java
@@ -0,0 +1,33 @@
+package com.deli.echolog.repository;
+
+import jakarta.persistence.EntityManager;
+import lombok.AllArgsConstructor;
+import lombok.RequiredArgsConstructor;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Repository;
+
+import java.time.LocalDate;
+import java.util.List;
+
+
+@RequiredArgsConstructor
+@Repository
+public class RecapRepository {
+
+    private final EntityManager em;
+
+    public List<Object[]> countEmotionTypesSince(LocalDate fromDate) {
+        return em.createQuery("""
+                SELECT e.emotionType, COUNT(e)
+                FROM Emotion e
+                JOIN e.diary d
+                WHERE d.writtenDate >= :fromDate
+                GROUP BY e.emotionType
+                ORDER BY e.emotionType
+            """, Object[].class)
+                .setParameter("fromDate", fromDate)
+                .getResultList();
+    }
+
+
+}

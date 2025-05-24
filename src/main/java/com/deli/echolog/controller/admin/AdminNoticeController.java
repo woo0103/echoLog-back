a753dae19ba1914d0f7e04ac72e610bc1555766d
wooooo <playjiwoo@naver.com>
2025-05-24 15:34:57 +0900

    member 조회할 때 페이징 처리

--- src/main/java/com/deli/echolog/controller/admin/AdminNoticeController.java
+++ src/main/java/com/deli/echolog/controller/admin/AdminNoticeController.java
@@ -3,6 +3,7 @@ package com.deli.echolog.controller.admin;
 import com.deli.echolog.service.NoticeService;
 import lombok.AllArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -13,4 +14,12 @@ import org.springframework.web.bind.annotation.RestController;
 public class AdminNoticeController {
 
     private final NoticeService noticeService;
+
+    // 공지사항 조회
+
+    // 공지사항 작성
+
+    // 공지사항 수정
+
+    // 공지사항 삭제
 }
